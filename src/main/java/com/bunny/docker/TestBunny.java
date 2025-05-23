43d868f748a020efca5df646cae99aa93e613156
BharathChandra <bharat.goud@73strings.com>
2025-05-23 14:22:45 +0530

    aded files commit

---
+++ src/main/java/com/bunny/docker/TestBunny.java
@@ -0,0 +1,15 @@
+package com.bunny.docker;
+
+public class TestBunny {
+
+    public TestBunny() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Bunny");
+    }
+
+    public void testMethod(){
+        System.out.println("test method called");
+    }
+}
